d455b3fcb2b3ed58d2eabebf3a3c961d5b873be0
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-12-02 05:21:05 +0900

    fix port number

--- app.go
+++ app.go
@@ -3,5 +3,5 @@ package main
 import infra "github.com/hukurou-s/user-auth-api-sample/infrastructure"
 
 func main() {
-	infra.Echo.Logger.Fatal(infra.Echo.Start(":1322"))
+	infra.Echo.Logger.Fatal(infra.Echo.Start(":1323"))
 }
