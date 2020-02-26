fbfbdb95452f65704ff26845588b43d470429019
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2020-02-26 18:08:36 +0900

    公開鍵と秘密鍵を読み込んで保持する処理を別パッケージに切り出し

---
+++ crypt/rsa.go
@@ -0,0 +1,67 @@
+package crypt
+
+import (
+	"crypto/rsa"
+	"github.com/dgrijalva/jwt-go"
+	"io/ioutil"
+)
+
+//type RSAKey struct {
+//	privateKey *rsa.PrivateKey
+//	publicKey *rsa.PublicKey
+//}
+
+var privateKey *rsa.PrivateKey
+var publicKey *rsa.PublicKey
+
+//func (keys *RSAKey) getPublicKey() *rsa.PublicKey {
+//	return keys.publicKey
+//}
+//
+//func (keys *RSAKey) getPrivateKey() *rsa.PrivateKey {
+//	return keys.privateKey
+//}
+
+func NewPrivateKey() *rsa.PrivateKey {
+	if privateKey == nil {
+		key, err := createPrivateKey()
+		if err != nil {
+			panic(err)
+		}
+		privateKey = key
+	}
+	return privateKey
+}
+
+func NewPublicKey() *rsa.PublicKey {
+
+	if publicKey == nil {
+		key, err := createPublicKey()
+		if err != nil {
+			panic(err)
+		}
+		publicKey = key
+	}
+	return publicKey
+}
+
+func createPublicKey() (key *rsa.PublicKey, err error) {
+	// 公開鍵を読み込む
+	pubKeyData, err := ioutil.ReadFile("./rsa/id_rsa.pub.pkcs8")
+	if err != nil {
+		return
+	}
+	key, err = jwt.ParseRSAPublicKeyFromPEM(pubKeyData)
+	return
+}
+
+func createPrivateKey() (key *rsa.PrivateKey, err error) {
+	// 秘密鍵を読み込み
+	privateKeyData, err := ioutil.ReadFile("./rsa/id_rsa")
+	if err != nil {
+		return
+	}
+
+	key, err = jwt.ParseRSAPrivateKeyFromPEM(privateKeyData)
+	return
+}
